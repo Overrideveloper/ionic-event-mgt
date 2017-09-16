62f6ef7fba6738cbebf8647c1a34acc884f2ef8d
overrideveloper <bansowisdom@gmail.com>
2017-09-16 13:45:24 +0100

    Added password reset functionality

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, Loading, LoadingController, NavController, Alert, AlertController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { EmailValidator } from '../../validators/email';
+import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the ResetPasswordPage page.
@@ -14,12 +18,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'reset-password.html',
 })
 export class ResetPasswordPage {
+  public resetPasswordForm:FormGroup;
+  public loading:Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public authProvider: AuthProvider, public alertCtrl: AlertController,
+   formBuilder: FormBuilder) {
+    this.resetPasswordForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResetPasswordPage');
+  resetPassword():void{
+    if(!this.resetPasswordForm.valid){
+      console.log(`Form isn't valid, value: ${this.resetPasswordForm.value}`);
+    }
+    else{
+      const email: string = this.resetPasswordForm.value.email;
+      this.authProvider.resetPassword(email).then( user => {
+        const alert:Alert = this.alertCtrl.create({
+          message: "Check your email for a password reset link",
+          buttons: [{
+            text: "Ok",
+            role: 'cancel',
+            handler: () => { this.navCtrl.pop() }
+          }]
+        });
+        alert.present()
+      }, error => {
+        const errorAlert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [{ text: "Ok", role: 'cancel' }]
+        });
+        errorAlert.present();
+      });
+    }
   }
-
 }
