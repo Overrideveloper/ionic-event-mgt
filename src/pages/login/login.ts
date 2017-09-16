f58cc9371ca0400794da794d78e782f13e078e6a
overrideveloper <bansowisdom@gmail.com>
2017-09-16 13:42:24 +0100

    Added login functionality

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, Loading, LoadingController, NavController, Alert, AlertController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { EmailValidator } from '../../validators/email';
+import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,12 +18,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  public loginForm:FormGroup;
+  public loading:Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public alertCtrl: AlertController, 
+  public authProvider: AuthProvider, formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
   }
+  
+    loginUser():void{
+      if(!this.loginForm.valid){
+        console.log(`Form isn't valid yet, value: ${this.loginForm.value}`);
+      }
+      else{
+        const email = this.loginForm.value.email;
+        const password = this.loginForm.value.password;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+        this.authProvider.loginUser(email, password).then( authData => {
+          this.loading.dismiss().then( () => {
+            this.navCtrl.setRoot(HomePage);
+          });
+        },
+        error => {
+          this.loading.dismiss().then( () => {
+            const alert:Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: "Ok", role: 'cancel'}]
+            });
+            alert.present()
+          });
+        });
+        this.loading = this.loadingCtrl.create();
+        this.loading.present();
+      }
+    }
 
+    goToSignUp():void{
+      this.navCtrl.push('SignupPage');
+    }
+
+    goToReset():void{
+      this.navCtrl.push('ResetPasswordPage');
+    }
 }
