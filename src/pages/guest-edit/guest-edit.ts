bd16c20a4d183f017be678f1ae3b552b8d257144
overrideveloper <bansowisdom@gmail.com>
2017-09-23 20:39:23 +0100

    Modified guest edit

--- src/pages/guest-edit/guest-edit.ts
+++ src/pages/guest-edit/guest-edit.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Alert, AlertController, ActionSheet, ActionSheetController } from 'ionic-angular';
+import { EventProvider } from '../../providers/event/event';
+import { Camera } from '@ionic-native/camera';
 
 /**
  * Generated class for the GuestEditPage page.
@@ -14,12 +16,100 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'guest-edit.html',
 })
 export class GuestEditPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public currentGuest:any;
+  public eventId: string;
+  public guestId: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public eventProvider: EventProvider, public alertCtrl: AlertController,
+    public camera: Camera, public actionSheetCtrl: ActionSheetController ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GuestEditPage');
+    this.eventId = this.navParams.get('eventId');
+    this.guestId = this.navParams.get('guestId');
+    this.eventProvider.guestDetails(this.eventId, this.guestId)
+      .on('value', guestSnapshot => {
+        this.currentGuest = guestSnapshot.val();
+      });
   }
 
+  updateName():void{
+    let alert:Alert = this.alertCtrl.create({
+      message: 'Edit name',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Enter name',
+          value: this.currentGuest.name
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.eventProvider.updateGuestName(this.eventId, this.guestId, data.name);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  takePicture():void{
+    this.camera.getPicture({
+      quality : 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: this.camera.EncodingType.PNG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.eventProvider.updateGuestImage(this.eventId, this.guestId, imageData);
+    }, error =>{
+      console.log("ERROR: " + JSON.stringify(error));
+    });
+  }
+
+  selectPicture():void{
+    this.camera.getPicture({
+      quality : 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: true,
+      encodingType: this.camera.EncodingType.PNG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.eventProvider.updateGuestImage(this.eventId, this.guestId, imageData);
+    }, error =>{
+      console.log("ERROR: " + JSON.stringify(error));
+    });
+  }
+
+  imageOptions():void{
+    let sheet:ActionSheet = this.actionSheetCtrl.create({
+      buttons:[
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture();
+          }
+        },
+        {
+          text: 'Select from Gallery',
+          handler: () => {
+            this.selectPicture();
+          }
+        }
+      ]
+    });
+    sheet.present();
+  }
 }
