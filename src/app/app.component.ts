431be46f5cd619c9ec6f4cdbbb79a9f96b3791cd
overrideveloper <bansowisdom@gmail.com>
2017-09-23 20:42:16 +0100

    Enabled prodMode and Camera Plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import firebase from 'firebase';
 import { TabsPage } from '../pages/tabs/tabs';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage :any;
-
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     firebase.initializeApp({
       apiKey: "AIzaSyBh1KUt3Aa6puJ8wRrtR7Cd1E7TkSy7UXU",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,11 @@ import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ProfilePage } from '../pages/profile/profile';
 import { EventListPage } from '../pages/event-list/event-list';
+import { AboutPage } from '../pages/about/about';
 import { AuthProvider } from '../providers/auth/auth';
 import { EventProvider } from '../providers/event/event';
 import { ProfileProvider } from '../providers/profile/profile';
 import { Camera } from '@ionic-native/camera';
-
-class CameraMock extends Camera
-{
-  getPicture(options){
-    return new Promise((resolve, reject) => {
-      resolve(`TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIG
-      J1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3a
-      GljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ug
-      b2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmV
-      yYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2
-      YgYW55IGNhcm5hbCBwbGVhc3VyZS4=`);
-    });
-  }
-}
 
 @NgModule({
   declarations: [
@@ -34,7 +21,8 @@ class CameraMock extends Camera
     HomePage,
     TabsPage,
     ProfilePage,
-    EventListPage
+    EventListPage,
+    AboutPage
   ],
   imports: [
     BrowserModule,
@@ -46,7 +34,8 @@ class CameraMock extends Camera
     HomePage,
     TabsPage,
     ProfilePage,
-    EventListPage
+    EventListPage,
+    AboutPage
   ],
   providers: [
     StatusBar,
@@ -55,7 +44,7 @@ class CameraMock extends Camera
     AuthProvider,
     EventProvider,
     ProfileProvider, 
-    {provide: Camera, useClass: CameraMock}
+    Camera
   ]
 })
 export class AppModule {}
