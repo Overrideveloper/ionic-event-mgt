354ac58255b7991df6a34f61b31d4640b0bd3f77
overrideveloper <bansowisdom@gmail.com>
2017-09-23 20:39:44 +0100

    Modified guest create

--- src/pages/guest-create/guest-create.ts
+++ src/pages/guest-create/guest-create.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheet, ActionSheetController } from 'ionic-angular';
 import { EventProvider } from '../../providers/event/event';
+import { Camera } from '@ionic-native/camera';
 
 /**
  * Generated class for the GuestCreatePage page.
@@ -17,8 +18,9 @@ import { EventProvider } from '../../providers/event/event';
 export class GuestCreatePage {
   public eventKey: string = '';
   public eventPrice: number = 0;
+  public guestPicture = null;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public eventProvider: EventProvider) {
+    public eventProvider: EventProvider, public camera: Camera, public actionSheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
@@ -27,10 +29,64 @@ export class GuestCreatePage {
   }
 
   addGuest(guestName:string):void{
-    this.eventProvider.addGuest(this.eventKey, guestName, this.eventPrice)
+    this.eventProvider.addGuest(this.eventKey, guestName, this.eventPrice, this.guestPicture)
         .then( newGuest => {
           this.navCtrl.setRoot('GuestListPage', { 'eventId': this.eventKey, 'eventPrice': this.eventPrice });
     });
   }
 
+  takePicture():void{
+    this.camera.getPicture({
+      quality : 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: this.camera.EncodingType.PNG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.guestPicture = imageData;
+    }, error =>{
+      console.log("ERROR: " + JSON.stringify(error));
+    });
+  }
+
+  selectPicture():void{
+    this.camera.getPicture({
+      quality : 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: true,
+      encodingType: this.camera.EncodingType.PNG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.guestPicture = imageData;
+    }, error =>{
+      console.log("ERROR: " + JSON.stringify(error));
+    });
+  }
+
+  imageOptions():void{
+    let sheet:ActionSheet = this.actionSheetCtrl.create({
+      buttons:[
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture();
+          }
+        },
+        {
+          text: 'Select from Gallery',
+          handler: () => {
+            this.selectPicture();
+          }
+        }
+      ]
+    });
+    sheet.present();
+  }
+  
 }
