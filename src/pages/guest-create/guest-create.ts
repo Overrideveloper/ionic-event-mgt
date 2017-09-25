45a38a4409accaa4ea921e25332d68cc02211aa0
overrideveloper <bansowisdom@gmail.com>
2017-09-25 14:10:57 +0100

    Modified guest create view

--- src/pages/guest-create/guest-create.ts
+++ src/pages/guest-create/guest-create.ts
@@ -46,7 +46,7 @@ export class GuestCreatePage {
       targetHeight: 500,
       saveToPhotoAlbum: true
     }).then(imageData => {
-      this.guestPicture = "data:image/png;base64," + imageData;
+      this.guestPicture = imageData;
     }, error =>{
       console.log("ERROR: " + JSON.stringify(error));
     });
@@ -62,7 +62,7 @@ export class GuestCreatePage {
       targetHeight: 500,
       saveToPhotoAlbum: true
     }).then(imageData => {
-      this.guestPicture = "data:image/png;base64," + imageData;
+      this.guestPicture = imageData;
     }, error =>{
       console.log("ERROR: " + JSON.stringify(error));
     });
